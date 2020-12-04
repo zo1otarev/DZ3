c9ef499770941cfc47955e60e5a88a2ca1ae2579
Zolotarev Daniil <zolotarew.daniil@mail.ru>
2020-12-04 20:18:30 +0300

    Калькулятор

---
+++ cmake-build-debug/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniil/calculator/RPN.cpp" "/home/daniil/calculator/cmake-build-debug/CMakeFiles/calculator.dir/RPN.cpp.o"
+  "/home/daniil/calculator/count.cpp" "/home/daniil/calculator/cmake-build-debug/CMakeFiles/calculator.dir/count.cpp.o"
+  "/home/daniil/calculator/main.cpp" "/home/daniil/calculator/cmake-build-debug/CMakeFiles/calculator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/RPN.cpp.o"
+  "CMakeFiles/calculator.dir/count.cpp.o"
+  "CMakeFiles/calculator.dir/main.cpp.o"
+  "calculator"
+  "calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
